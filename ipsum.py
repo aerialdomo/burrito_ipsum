b96449d160577d9e1d78b4d7881537c690df9181
aerialdomo <christinaliu26@gmail.com>
2013-05-30 17:27:48 -0700

    corpus will inport into ipsum, working on breaking it apart and putting same words into dictionaries

---
+++ ipsum.py
@@ -0,0 +1,20 @@
+import sys
+import random
+
+def make_chains(input_text):
+	text_dict = {}
+	input_text = input_text.split()
+
+	# for words in input_text:
+		
+	return ipsum
+
+def main(argv):
+	args = sys.argv
+	script, filename = argv
+	input_text = open(filename).read()
+	ipsum = make_chains(input_text)
+	print ipsum
+
+if __name__ == "__main__":
+	main(sys.argv)	
